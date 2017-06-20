0538dbe2fab7528165a1c8c3ce7bf6fa7bdf8937
Oliver <zhliu@tecomtech.com>
2017-06-20 16:05:10 +0800

    add multi monster
    support multi monster at same time

--- js/index.js
+++ js/index.js
@@ -3,11 +3,7 @@ var me = new Player({
         HP: 10,
         point: [5, 6]
     }),
-    monster = new Player({
-        name: "哥布林",
-        HP: 5,
-        point: [6]
-    }),
+    monster = new Array(3),
     leftRollTimes = 3,
     userDices = new Array(6),
     logAppend = function(text){
@@ -38,13 +34,10 @@ var me = new Player({
         }
 
         updateLock();
-    }
+    };
 
 $("#myName").text(me.name);
 $("#myHP").text("HP: " + me.currentHP);
-
-$("#monsterName").text(monster.name);
-$("#monsterHP").text("HP: " + monster.currentHP);
 
 logAppend("初始化中...");
 logAppend("初始化完毕！");
@@ -52,6 +45,84 @@ logAppend("初始化完毕！");
 for(var i = 0; i < userDices.length; i ++){
     userDices[i] = new Dice();
 }
+
+
+var updateMonsterList = function(monster){
+    var monsterNameTR = $("<tr></tr>"),
+        monsterHPTR = $("<tr></tr>");
+
+    for(var i = 0; i < monster.length; i ++){
+        var monsterNameTD = $("<td index='" + i + "'>" + monster[i].name + "</td>"),
+            monsterHPTD = $("<td index='" + i + "'>" + monster[i].currentHP + "/" + monster[i].maxHP + "</td>"),
+            clickHandler = function(){
+                var index = parseInt($(this).attr("index")),
+                    list = new Array(6);
+                for(var j = 0; j < list.length; j ++){
+                    list[j] = userDices[j].dump();
+                }
+
+                var damanage = me.damanage(list);
+
+                monster[index].handleDamanage(damanage);
+                logAppend(me.name + "对" + monster[index].name + "造成了" + damanage + "点伤害");
+
+                for(var j = 0; j < userDices.length; j ++){
+                    userDices[j].locked = false;
+                }
+                leftRollTimes = 3;
+                rollDice();
+
+                if(monster[index].isDied()){
+                    logAppend(monster[index].name + "挂了");
+                }
+
+                for(var m = 0; m < monster.length; m ++){
+                    for(var j = 0; j < list.length; j ++){
+                        list[j] = new Dice();
+                        list[j].roll();
+                    }
+
+                    if(!monster[m].isDied()){
+                        var damanage = monster[m].damanage(list);
+
+                        me.handleDamanage(damanage);
+                        logAppend(monster[m].name + "对" + me.name + "造成了" + damanage + "点伤害");
+                    }
+
+                    if(me.isDied()){
+                        logAppend(me.name + "挂了");
+                    }
+
+                    $("#myName").text(me.name);
+                    $("#myHP").text("HP: " + me.currentHP);
+                }
+                
+                updateMonsterList(monster);
+            };
+
+        if(monster[i].isDied()){
+            monsterNameTD.css("backgroundImage", "url(img/dead.png)");
+        }
+
+        monsterNameTD.click(clickHandler);
+        monsterHPTD.click(clickHandler);
+
+        monsterNameTR.append(monsterNameTD);
+        monsterHPTR.append(monsterHPTD);
+    }
+
+    $(".monsterDetailZone").html(monsterNameTR);
+    $(".monsterDetailZone").append(monsterHPTR);
+}
+
+for(var i = 0; i < monster.length; i ++){
+    monster[i] = new Player({
+        name: "哥布林" + i,
+        HP: 5,
+        point: [6]
+    });
+}
+updateMonsterList(monster);
 
 $(".diceDiv").each(function(){
     $(this).click(function(){
@@ -67,44 +138,3 @@ $("#roll").click(function(){
     rollDice();
 });
 
-$("#attack").click(function(){
-    var list = new Array(6);
-    for(var i = 0; i < list.length; i ++){
-        list[i] = userDices[i].dump();
-    }
-
-    var damanage = me.damanage(list);
-
-    monster.handleDamanage(damanage);
-    logAppend(me.name + "对" + monster.name + "造成了" + damanage + "点伤害");
-
-    for(var i = 0; i < userDices.length; i ++){
-        userDices[i].locked = false;
-    }
-    leftRollTimes = 3;
-    rollDice();
-
-    if(monster.isDied()){
-        logAppend(monster.name + "挂了");
-    }
-
-    $("#monsterName").text(monster.name);
-    $("#monsterHP").text("HP: " + monster.currentHP);
-
-    for(var i = 0; i < list.length; i ++){
-        list[i] = new Dice();
-        list[i].roll();
-    }
-
-    var damanage = monster.damanage(list);
-
-    me.handleDamanage(damanage);
-    logAppend(monster.name + "对" + me.name + "造成了" + damanage + "点伤害");
-
-    if(me.isDied()){
-        logAppend(me.name + "挂了");
-    }
-
-    $("#myName").text(me.name);
-    $("#myHP").text("HP: " + me.currentHP);
-});
