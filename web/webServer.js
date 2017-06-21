fc926dcdc1f4c1bb6a56d30f107a4d83197e7137
Oliver <zhliu@tecomtech.com>
2017-06-21 11:16:04 +0800

    fix can not find proto.js bug

--- web/webServer.js
+++ web/webServer.js
@@ -1,4 +1,4 @@
-const PORT = 8080;
+const PORT = 8090;
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
@@ -8,7 +8,9 @@ const path = require("path");
 var server = http.createServer(function (request, response) {
     var pathname = url.parse(request.url).pathname;
     pathname = (pathname == "/")?"/index.html":pathname;
+    pathname = (pathname.substr(-8) == "proto.js")?("../../" + pathname):pathname;
     var realPath = path.join("assets", pathname);
+
     // console.log(realPath);
     var ext = path.extname(realPath);
     ext = ext ? ext.slice(1) : "unknown";
