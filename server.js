dcc43f201ea4f634c827be5f02c5047e06af4494
Oliver <zhliu@tecomtech.com>
2017-06-21 11:31:42 +0800

    Adjust the code structure
    combine the web server and ws server into one file

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const PORT = 8090;
+const WEBPORT = 8090;
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
 const mine = require("./mine").types;
 const path = require("path");
 
+const WSPORT = 8000;
+const WebSocket = require('ws'); // https://github.com/websockets/ws
+const wss = new WebSocket.Server({ port: WSPORT });
+const protobuf = require("protobufjs"); // https://github.com/dcodeIO/protobuf.js
+
+const handler = require("./handler")
+
 var server = http.createServer(function (request, response) {
     var pathname = url.parse(request.url).pathname;
     pathname = (pathname == "/")?"/index.html":pathname;
-    pathname = (pathname.substr(-8) == "proto.js")?("../../" + pathname):pathname;
-    var realPath = path.join("assets", pathname);
+    var realPath = path.join("./", pathname);
 
-    // console.log(realPath);
     var ext = path.extname(realPath);
     ext = ext ? ext.slice(1) : "unknown";
     fs.exists(realPath, function (exists) {
@@ -43,5 +48,39 @@ var server = http.createServer(function (request, response) {
         }
     });
 });
-server.listen(PORT);
-console.log("WEB server runing at port: " + PORT + ".");
+server.listen(WEBPORT);
+console.log("WEB server runing at port: " + WEBPORT + ".");
+
+protobuf.load("./util/proto/test.proto", function(err, root) {
+    if (err)
+        throw err;
+    var TestModel = root.lookupType("test.testModel");
+    var sendMsg = function(ws, msg){
+        var errMsg = TestModel.verify(msg);
+        if (errMsg)
+            throw Error(errMsg);
+
+        var message = TestModel.create(msg),
+            buffer = TestModel.encode(message).finish();
+
+        ws.send(buffer);
+    }
+
+    wss.on("connection", function(ws){
+        ws.on("message", function(message){
+            var testVal = TestModel.decode(message);
+     
+            console.log(JSON.stringify(testVal));
+        });
+
+        sendMsg(ws, {
+            name: "test",
+            age: 28,
+            male: true
+        })
+    });
+
+    console.log("WS server runing at port: " + WSPORT + ".");
+});
+
+
